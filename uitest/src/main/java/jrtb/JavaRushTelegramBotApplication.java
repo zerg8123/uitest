5d83ab6d19a9880ad3e241bc75d962a36ed4d0fb
[sergey] <example@gmail.com>
2021-09-24 11:27:48 +0300

    add skeleton telegram bot

---
+++ uitest/src/main/java/jrtb/JavaRushTelegramBotApplication.java
@@ -0,0 +1,11 @@
+package jrtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRushTelegramBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRushTelegramBotApplication.class, args);
+    }
+}
